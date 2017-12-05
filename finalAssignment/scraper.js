90f2ab0de66c5ef3c734dbef9df26f5e0900b7ad
Charley Ballantyne <charley@twin-one.com>
2017-12-05 14:36:41 -0800

    Scraper is working for current conditions, need to figure out how to import it into the server.js file

---
+++ finalAssignment/scraper.js
@@ -0,0 +1,52 @@
+const cheerio = require('cheerio');
+const request = require('request');
+
+//Google search results
+let url = "http://orca.bcferries.com:8080/cc/marqui/at-a-glance.asp";
+
+request(url, function (error, response, body) {
+    if (!error) {
+        let $ = cheerio.load(body);
+        let ferryRouteTswToSwb = '';
+        let sailingTswToSwb = '';
+        let percentFullTswToSwb = '';
+        let carWaitsTswToSwb = '';
+        let nextSailingTswToSwb = '';
+        let nextPercentFullTswToSwb = '';
+
+        $('#tblLayout > tbody > tr > td > table > tbody > tr > td > table:nth-child(8) > tbody > tr:nth-child(2) > td:nth-child(1)').each(function () {
+            ferryRouteTswToSwb = ($(this).text())
+        });
+
+        $('#tblLayout > tbody > tr > td > table > tbody > tr > td > table:nth-child(8) > tbody > tr:nth-child(2) > td:nth-child(2) > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > a').each(function () {
+            sailingTswToSwb = ($(this).text())
+        });
+
+        $('#tblLayout > tbody > tr > td > table > tbody > tr > td > table:nth-child(8) > tbody > tr:nth-child(2) > td:nth-child(2) > div > table > tbody > tr:nth-child(1) > td:nth-child(2)').each(function () {
+            percentFullTswToSwb = ($(this).text())
+        });
+
+        $('#tblLayout > tbody > tr > td > table > tbody > tr > td > table:nth-child(8) > tbody > tr:nth-child(2) > td:nth-child(3) > div').each(function () {
+            carWaitsTswToSwb = ($(this).text())
+        });
+
+        $('#tblLayout > tbody > tr > td > table > tbody > tr > td > table:nth-child(8) > tbody > tr:nth-child(2) > td:nth-child(2) > div > table > tbody > tr:nth-child(2) > td:nth-child(1) > a').each(function () {
+            nextSailingTswToSwb = ($(this).text())
+        });
+
+        $('#tblLayout > tbody > tr > td > table > tbody > tr > td > table:nth-child(8) > tbody > tr:nth-child(2) > td:nth-child(2) > div > table > tbody > tr:nth-child(2) > td:nth-child(2)').each(function () {
+            nextPercentFullTswToSwb = ($(this).text())
+        });
+
+        console.log("\nBC Ferries Data \n -----------------------------");
+        console.log(ferryRouteTswToSwb);
+        console.log(sailingTswToSwb);
+        console.log(percentFullTswToSwb);
+        console.log(carWaitsTswToSwb);
+        console.log(nextSailingTswToSwb);
+        console.log(nextPercentFullTswToSwb);
+
+    } else {
+        console.log("We've encountered an error: " + error);
+    }
+});
